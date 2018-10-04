7c9939ce6ed8a90cee12a68181fa30478924044e
Gil <gil.mardon@gmail.com>
2018-10-04 21:07:57 +0200

    Add apikey support

--- node_helper.js
+++ node_helper.js
@@ -4,9 +4,9 @@
  * License: GNU General Public License */
 
 var NodeHelper = require("node_helper");
-const restling = require('restling');
-const Promise = require('bluebird');
-const _ = require('lodash');
+const restling = require("restling");
+const Promise = require("bluebird");
+const _ = require("lodash");
 
 module.exports = NodeHelper.create({
 
@@ -42,20 +42,23 @@ module.exports = NodeHelper.create({
     getData: function () {
         let self = this;
         let returnedData = {
-            nhits : 0,
-            records : []
+            nhits: 0,
+            records: []
         };
         let RestArgs = {
             rejectUnauthorized: false
         };
 
-        return Promise.each(self.config.lines, function(line) {
+        return Promise.each(self.config.lines, function (line) {
             let urlApi = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=prochains-passages-des-lignes-de-bus-du-reseau-star-en-temps-reel&sort=nomcourtligne&facet=idligne&facet=nomcourtligne&facet=sens&facet=destination&facet=precision&facet=nomarret&refine.idligne=" +
-            line.line +
-            "&refine.nomarret=" +
-            line.stop +
-            "&refine.destination=" +
-            line.destination;
+                line.line +
+                "&refine.nomarret=" +
+                line.stop +
+                "&refine.destination=" +
+                line.destination;
+            if (!_.isEmpty(self.config.apikey)) {
+                urlApi += "&apikey=" + self.config.apikey;
+            }
 
             return restling.get(urlApi, RestArgs).then(function (response) {
                 if (response.response.statusCode === 401) {
@@ -66,17 +69,17 @@ module.exports = NodeHelper.create({
                 }
                 delete response.data.parameters;
                 delete response.data.facet_groups;
-                
+
                 returnedData.nhits += response.data.nhits;
                 returnedData.records = returnedData.records.concat(response.data.records);
-                
-                return Promise.resolve();
-            }).then(function() {            
-                let sortedData = { 
-                    records : _.sortBy(returnedData.records, 'fields.arrivee'),
-                    nhits : returnedData.nhits 
-                };
 
+                return Promise.resolve();
+            }).then(function () {
+                let sortedData = {
+                    records: _.sortBy(returnedData.records, 'fields.arrivee'),
+                    nhits: returnedData.nhits
+                };
+                console.log("returnedData:", returnedData);
                 return self.sendSocketNotification("DATA", sortedData);
             }).catch(function (e) {
                 console.log("Communications error:", e.message);
